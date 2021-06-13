5c79227938876abaea416d02860da633d6c209aa
scriptkiddie-hanze <56271830+scriptkiddie-hanze@users.noreply.github.com>
2021-06-13 17:15:03 +0200

    V1.1
    Changed generated config path for compatibility with new minions

--- Nagios/createNAGIOSconfig.sh
+++ Nagios/createNAGIOSconfig.sh
@@ -3,7 +3,7 @@
 HOSTNAME=$(hostname)
 IPADDRESS=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 
-cat > /tmp/nagios/config.cfg << EOF
+cat > /tmp/config.cfg << EOF
 
 define host {
         use                   linux-server
